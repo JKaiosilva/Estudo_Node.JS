3cc02d35571e09ab71878c9d61a74e017a1aaf8d
Kaio Silva <106553278+JKaiosilva@users.noreply.github.com>
2023-01-23 19:57:09 -0400

    Resolvido conexão com mongoose

--- trabalhandoComMongo/aprendendo.js
+++ trabalhandoComMongo/aprendendo.js
@@ -1,22 +1,62 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose
-  .connect("mongodb://localhost/aprendendo", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,})
+
+// COnfig Mongoose
+
+  mongoose.set('strictQuery', true)
+    mongoose.connect("mongodb://0.0.0.0:27017/aprendendo", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,})
+          .then(() => {
+            console.log("Mongo conectado...");
+        })
+          .catch((err) => {
+            console.log("Deu erro aqui ó: " + err);
+        });
+
+// Model - Usuários
+
+const UsuarioSchema = mongoose.Schema({
+  nome: {
+    type: String,
+    require: true
+  },
+  sobrenome: {
+    type: String,
+    require:true
+  },
+  email: {
+    type: String,
+    require: true
+  },
+  idade: {
+    type: Number,
+    require: true
+  },
+  pais: {
+    type: String
+  }
+})
+
+
+// Nomeando Collection
+
+  mongoose.model('usuarios', UsuarioSchema)
+
+  const Kaio = mongoose.model('usuarios')
+
+  new Kaio({
+    nome: "kaio",
+    sobrenome: 'Silva',
+    email: "teste@test.mail",
+    idade: 24,
+    pais: 'Brasil'
+  }).save()
   .then(() => {
-    console.log("Mongo conectado...");
+    console.log("Usuario salvo")
   })
   .catch((err) => {
-    console.log("Deu erro aqui ó: " + err);
-  });
-
-
-
-//mongoose.connect('mongodb://localhost/aprendendo', {useNewUrlParser: true, useUnifiedTopology: true}).then(()=> {
-//    console.log('MongoDB conectado')
-//}).catch((err)=> {
- //   console.log('Erro ao se conectar: ' + err)
-//})
+    console.log("Erro " + err)
+  })
 
